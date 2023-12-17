a05d23f5f30f60a0286b145a3a02a1bc2811d144
Sasdarl <77890964+Sasdarl@users.noreply.github.com>
2023-12-17 00:01:11 -0500

    Add English patch compatibility and fix output paths without slash at the end

--- PBPS2bin.py
+++ PBPS2bin.py
@@ -102,7 +102,9 @@ def unpack(buf, folder, model):
                 compressed >>= 13
                 idArray.extend(wu08(buf[fileOffset+10]))
                 if compressed == 1:
-                    fileData = deZLib(buf,dataOffset+4,dataSize-4) # Decompress if needed
+                    paddedSize = dataSize + 0x800-(dataSize%0x800) # The English patch messed up the sizes
+                    tempBuf = buf[dataOffset+4:dataOffset+paddedSize] # It's easier to just bake the fix in
+                    fileData = deZLib(tempBuf,0,len(tempBuf)) # Decompress if needed
                 else:
                     fileData = buf[dataOffset:dataOffset+dataSize]
             if len(fileData) > 0:
@@ -248,13 +250,13 @@ parser.add_argument("inpath", help="File Input (BIN)") # But I do
 parser.add_argument("-o", "--outpath", help="Optional. The name used for the output folder or file.")
 parser.add_argument("-nc", "--nocompress", action="store_true", help="Optional. Disables ZLib compression on files within the BIN.") # For if you want the chunkiest possible game directory
 parser.add_argument("-m", "--model", action="store_true", help="Optional. Indicates a BIN file is formatted for model files.") # There are BIN files inside BIN files. It gets weirder
-parser.add_argument("-qb", "--qbextensions", action="store_true", help="Optional. Gives PGM and DAT extensions in place of TEX/TX2 and LXE.") # For compatibility and nostalgia
+parser.add_argument("-q", "--qbextensions", action="store_true", help="Optional. Gives PGM and DAT extensions in place of TEX/TX2 and LXE.") # For compatibility and nostalgia
 args = parser.parse_args()
 if Path(args.inpath).is_file() and not Path(args.inpath).is_dir(): # BIN input is assumed   
     with open(args.inpath, "rb") as input_file:
         input_file_buffer = bytearray( input_file.read() )
         if args.outpath:
-            outpath = args.outpath
+            outpath = args.outpath + "/"
         else:
             if args.model:
                 outpath = (f"{Path(args.inpath).parent}/model-{Path(args.inpath).stem}/")
