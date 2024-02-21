cd9e9dcacf23cb7c712f1ea59a491ab0150f36a7
Sasdarl <77890964+Sasdarl@users.noreply.github.com>
2024-02-21 10:20:15 -0500

    Split extract into submethods and added single-file extraction

--- PBPS2bin.py
+++ PBPS2bin.py
@@ -64,115 +64,192 @@ def numsort(str): # Because default numeric string sorting is the worst
     for c in str:
         if c.isnumeric():
             numstr += c
-    for d in range(20-len(numstr)): # We're basically just standardizing the lengths
-        numstr = "0" + numstr # God help those who decide to put 18 zeroes in front of a filename
-    return numstr
+    return int(numstr) # Let's just get an integer out of all of the numeric characters
 
-def unpack(buf, folder, model, useFilelist, qbFile):
-    folderCount = ru32(buf,0x00) # Number of folders
-    effModel = False
-    if model and ru32(buf,0x08) != 0x20031205:
-        effModel = True
-    listPositions = []
-    listNames = []
-    if useFilelist and not model:
-        with open("./filelist.txt") as filelist:
-            lines = filelist.readlines()
-            for line in lines:
-                if line != "\n":
-                    listPositions.append(line.rsplit(":",1)[0]) # Store positions and names in two arrays
-                    line = line.rsplit(":",1)[1]
-                    listNames.append(line.rsplit("\n")[0].strip("\""))
+def parse_filelist(infile,folderCount): # Convert the filelist into a two-dimensional array
+    outarray = []
+    for i in range(folderCount): # Set up inner arrays beforehand
+        outarray.append([])
+    with open(infile) as filelist:
+        lines = filelist.readlines() # Get each text line from the file
+        for line in lines:
+            if line != "\n": # Make sure the line isn't empty
+                linearray = [] # [Folder num, file num, name folder, name stem]
+                filePos = line.split(":",1)[0] # Everything before ":" (file position)
+                fileName = line.split(":",1)[1] # Everything after ":" (file name)
+                fileName = fileName.strip("\n\"") # Remove quotation marks from name
+                linearray += filePos.split("/") # Split position by "/" and add both elements
+                linearray += fileName.rsplit("/",1) # Split name by last "/" and add both elements
+                outarray[int(linearray[0])].insert(int(linearray[1]),linearray) # Add to the appropriate array
+    return outarray
+
+def parse_header(buf,folderCount,effModel=False): # Convert the header into a two-dimensional array
+    outarray = []
+    fileAdjust = False # 12/0 and 27/0 are listed at the wrong locations in the header
     if effModel:
         folderOffset = 0x04
-        dataOffset = folderCount*0x10+0x04
+        dataOffset = folderOffset+(folderCount*0x10)
     else:
-        idArray = bytearray(0)
         folderOffset = 0x10
-        fileOffset = folderCount*0x10+0x10
     for i in range(folderCount):
+        outarray.append([]) # Set up inner array
         if effModel:
             fileCount = 4
         else:
-            dataOffset = ru32(buf,folderOffset)
+            fileOffset = ru32(buf,folderOffset) # This only needs to exist in files with variable folders
             fileCount = ru32(buf,folderOffset+4)
-            print(f"Folder {i}: {fileCount} files")
-        outfolder = (f"{folder}{i}/")
-        if not useFilelist or model: # Make numeric directories
-            Path(outfolder).mkdir(parents=True,exist_ok=True) # If we have a file list, we don't need them
         for j in range(fileCount):
+            filearray = [] # [File offset, file size, compressed, ID]
             if effModel:
-                fileOffset = folderOffset+j*0x04
-                dataSize = ru32(buf,fileOffset)
-                if dataSize > 0:
-                    fileData = buf[dataOffset:dataOffset+dataSize]
-                else:
-                    fileData = []
-                dataOffset += dataSize
+                filearray.append(dataOffset)
+                filearray.append(ru32(buf,fileOffset))
+                filearray.append(0) # There's no flag for this
+                filearray.append(0) # This isn't in the format
+                dataOffset += ru32(fileOffset)
+                fileOffset += 0x04
             else:
                 dataOffset = ru32(buf,fileOffset)
-                dataSize = ru32(buf,fileOffset+4)
+                if i == 12 and j == 0 and dataOffset == 0x29D2000:
+                    fileOffset = 0x10+(0x10*27) # The aforementioned header fix
+                    fileAdjust = True
+                elif i == 27 and j == 0 and dataOffset == 0x52A800:
+                    fileOffset = 0x10+(0x10*12) # We check the offset because in any other case than vanilla we'll have already fixed it
+                    fileAdjust = True
                 compressed = (ru16(buf,fileOffset+8)&0x2000) # Check compression
                 compressed >>= 13
-                idArray.extend(wu08(buf[fileOffset+10]))
-                if compressed == 1:
-                    paddedSize = dataSize + 0x800-(dataSize%0x800) # The English patch messed up the sizes
-                    tempBuf = buf[dataOffset+4:dataOffset+paddedSize] # It's easier to just bake the fix in
-                    fileData = deZLib(tempBuf,0,len(tempBuf)) # Decompress if needed
-                else:
-                    fileData = buf[dataOffset:dataOffset+dataSize]
-            if len(fileData) > 0:
-                outext = determineExtension(fileData,qbFile)
-                if effModel:
-                    if j == 0: # Might as well name them
-                        outpath = (f"{outfolder}model.{outext}")
-                    elif j == 1:
-                        outpath = (f"{outfolder}texture.{outext}")
-                    elif j == 2: # If someone figures out what these are, let me know
-                        outpath = (f"{outfolder}unknown.{outext}")
-                    else:
-                        outpath = (f"{outfolder}unknown{j-1}.{outext}")
-                else:
-                    outpath = (f"{outfolder}{j}.{outext}")
-                    if i == 12 and j == 0 and dataOffset == 0x29D2000: # I wish I knew why too
-                        outpath = (f"{folder}27/{j}.{outext}")
-                        if not useFilelist:
-                            Path(f"{folder}27/").mkdir(parents=True,exist_ok=True)
-                    if i == 27 and j == 0 and dataOffset == 0x52A800:
-                        outpath = (f"{folder}12/{j}.{outext}")
-                    if useFilelist and not model:
-                        found = False
-                        search = (f"{i}/{j}")
-                        if i == 12 and j == 0 and dataOffset == 0x29D2000:
-                            search = "27/0"
-                        if i == 27 and j == 0 and dataOffset == 0x52A800:
-                            search = "12/0"
-                        for k in range(len(listPositions)):
-                            if listPositions[k] == search: # Search for our file position
-                                outpath = (f"{folder}{listNames[k]}") # Use the corresponding file name
-                                Path(outpath).parent.mkdir(parents=True,exist_ok=True) # Make the folder required
-                                listPositions.pop(k) # Remove the position and name from the array
-                                listNames.pop(k)
-                                found = True
-                                break
-                        if found == False:
-                            Path(outfolder).mkdir(parents=True,exist_ok=True) # Otherwise go with the original plan
-                with open(outpath, "wb") as outfile:
+                filearray.append(dataOffset)
+                filearray.append(ru32(buf,fileOffset+4))
+                filearray.append(compressed)
+                filearray.append(ru08(buf,fileOffset+10))
+                if fileAdjust:
+                    fileOffset = 0x10+(0x10*i)
+                fileOffset += 0x10
+            outarray[i].append(filearray)
+        folderOffset += 0x10
+    return outarray
+
+def get_file_data(buf,offset,size,compressed=0): # Get (and decompress if necessary) a file as a bytearray
+    if compressed == 1:
+        paddedSize = size + 0x800-(size%0x800) # The English patch messed up the sizes
+        tempBuf = buf[offset+4:offset+paddedSize] # It's easier to just bake the fix in
+        fileData = deZLib(tempBuf,0,len(tempBuf)) # Decompress if needed
+        if not len(fileData) == ru32(buf,offset):
+            print(f"Incorrect file size at {offset}! Attempting to ignore...")
+    else:
+        if size > 0:
+            fileData = buf[offset:offset+size]
+        else:
+            fileData = []
+    return fileData
+
+def get_file_name(buffer,folder,file,filelist=[],effModel=False,qbFile=False,noFolder=False): # Determine the path where the file should be stored
+    outext = determineExtension(buffer,qbFile)
+    if effModel:
+        if file == 0: # Might as well name them
+            outpath = (f"model.{outext}")
+        elif file == 1:
+            outpath = (f"texture.{outext}")
+        elif file == 2: # If someone figures out what these are, let me know
+            outpath = (f"unknown.{outext}")
+        else:
+            outpath = (f"unknown{file-1}.{outext}")
+        if not noFolder:
+            outpath = (f"{folder}/{outpath}")
+    else:
+        if len(filelist) > 0:
+            outpath = (f"{filelist[folder][file][3]}")
+            if not noFolder:
+                outpath = (f"{filelist[folder][file][2]}/{outpath}")
+        else:
+            if not noFolder:
+                outpath = (f"{folder}/{file}.{outext}")
+            else:
+                outpath = (f"{folder}_{file}.{outext}")
+    return outpath
+
+def extract_file(buf, folder, lookFolder, lookFile, model=False, useFilelist=True, qbFile=False):
+    folderCount = ru32(buf,0x00) # Number of folders
+    effModel = False
+    if lookFolder > (folderCount - 1): # Does the folder exist?
+        print(f"Invalid file {lookFolder}/{lookFile}: There are only {folderCount} folders in this file!")
+        return -1
+    if model and ru32(buf,0x08) != 0x20031205: # 9/0 (game/eff/eff.bin) is a unique case
+        effModel = True
+
+    lines = [] # Set up our filelist
+    if useFilelist and not model:
+        lines = parse_filelist("./filelist.txt",folderCount)
+
+    files = parse_header(buf,folderCount,effModel) # Set up our directory structure
+    
+    if lookFile > len(files[lookFolder]): # Does the file exist?
+        print(f"Invalid file {lookFolder}/{lookFile}: There are only {len(files[lookFolder])} files in this folder!")
+        return -1
+    print(f"Accessing folder {lookFolder} ({len(files[lookFolder])} file(s))...")
+    getFile = files[lookFolder][lookFile]
+    fileData = get_file_data(buf,getFile[0],getFile[1],getFile[2]) # Put the file data in a buffer
+
+    if len(fileData) > 0: # But does the file ACTUALLY exist?
+        outpath = get_file_name(fileData,lookFolder,lookFile,lines,effModel,qbFile,True) # Get the best file name
+        Path(f"{folder}{outpath}").parent.mkdir(parents=True,exist_ok=True) # Make the folder required
+        with open(f"{folder}{outpath}", "wb") as outfile:
+            for byte in fileData:
+                outfile.write(struct.pack("B",byte))
+        print(f"Successfully extracted to {outpath}.")
+        return 0
+    else:
+        print(f"Invalid file {lookFolder}/{lookFile}: The file you are looking for does not exist!")
+        return -1
+
+def unpack(buf, folder, model=False, useFilelist=True, qbFile=False):
+    folderCount = ru32(buf,0x00) # Number of folders
+    idArray = bytearray(0) # For storing an unknown and unused value tentatively dubbed the ID
+    effModel = False
+    if model and ru32(buf,0x08) != 0x20031205: # 9/0 (game/eff/eff.bin) is a unique case
+        effModel = True
+
+    lines = [] # Set up our filelist
+    if useFilelist and not model:
+        lines = parse_filelist("./filelist.txt",folderCount)
+
+    files = parse_header(buf,folderCount,effModel) # Set up our directory structure
+
+    getFile = []
+    for i in range(folderCount):
+        print(f"Folder {i}: {len(files[i])} files")
+        for j in range(len(files[i])):
+            getFile = files[i][j]
+            fileData = get_file_data(buf,getFile[0],getFile[1],getFile[2]) # Put the file data in a buffer
+            idArray.extend(wu08(getFile[3])) # Also add the ID to the corresponding array
+            if len(fileData) > 0: # Does the file exist?
+                curFolder = i
+                if i == 12 and j == 0 and getFile[0] == 0x29D2000: # We already got the switched file
+                    curFolder = 27 # But we still need the switched name
+                if i == 27 and j == 0 and getFile[0] == 0x52A800:
+                    curFolder = 12
+                outpath = get_file_name(fileData,curFolder,j,lines,effModel,qbFile) # Get the best file name
+                Path(f"{folder}{outpath}").parent.mkdir(parents=True,exist_ok=True) # Make the folder required
+                with open(f"{folder}{outpath}", "wb") as outfile:
                     for byte in fileData:
                         outfile.write(struct.pack("B",byte))
-                if not effModel: # Just so the user knows we're not stuck
-                    fileOffset += 0x10
-                    if j >= 499 and (j+1)%100 == 0:
-                        print(f"Please wait. {j+1} files complete...", end="\r", flush=True)
-                    if j > 499 and j == fileCount - 1:
-                        print(f"Please wait. {j+1} files complete.  ")
-        folderOffset += 0x10
+            if not effModel: # Progress report for folders with over 500 files
+                if j >= 499 and (j+1)%100 == 0: # Just so the user knows we're not stuck
+                    print(f"Please wait. {j+1} files complete...", end="\r", flush=True)
+                if j > 499 and j == len(files[i]) - 1:
+                    print(f"Please wait. {j+1} files complete.  ")
+
     if not effModel:
         outid = (f"{folder}filelist.id")
-        with open(outid, "wb") as id_file: # These aren't necessary but until I figure out how they're calculated
-            id_file.write(idArray) # this will have to do
+        with open(outid, "wb") as id_file: # Write the ID array
+            id_file.write(idArray)
             id_file.close()
     return 0
+
+def modify_header(buf,folder,file,newsize,effModel=False):
+    return -1
+
+def rebuild_header(headerarray,effModel=False):
+    return -1
 
 def add_file(input,databuffer,fileheader,folder,compress=True):
     fileheader.extend(wu32(len(databuffer)))
@@ -195,7 +272,7 @@ def add_file(input,databuffer,fileheader,folder,compress=True):
     if remainder != 0:
         databuffer.extend(bytes(0x800-remainder))
 
-def rebuild(folder,output,compress,useFilelist):
+def rebuild(folder,output,compress=True,useFilelist=True):
     folders = 0
     totalfiles = 0
     databuffer = bytearray(0)
@@ -321,8 +398,10 @@ parser.add_argument("inpath", help="File Input (BIN/Folder)") # But I do
 parser.add_argument("-o", "--outpath", help="Optional. The name used for the output folder or file.")
 parser.add_argument("-nc", "--nocompress", action="store_true", help="Optional. Disables ZLib compression on files within the BIN.") # For if you want the chunkiest possible game directory
 parser.add_argument("-m", "--model", action="store_true", help="Optional. Indicates a BIN file is formatted for model files.") # There are BIN files inside BIN files. It gets weirder
-parser.add_argument("-q", "--qbextensions", action="store_true", help="Optional. Gives PGM and DAT extensions in place of TEX/TX2 and LXE.") # For compatibility and nostalgia
+parser.add_argument("-qb", "--qbextensions", action="store_true", help="Optional. Gives PGM and DAT extensions in place of TEX/TX2 and LXE.") # For compatibility and nostalgia
 parser.add_argument("-nl", "--nolist", action="store_true", help="Optional. Ignores the provided file list, if available.") # Ditto
+parser.add_argument("-fo", "--folder", type=int, default="-1", help="Optional. Only extracts files from the desired folder.") # Ditto
+parser.add_argument("-fi", "--file", type=int, default="-1", help="Optional. Only extracts the desired zero-indexed file from a folder.") # Ditto
 args = parser.parse_args()
 
 if not Path("./filelist.txt").is_file(): # If we don't have a file list, we don't have it
@@ -337,11 +416,29 @@ if Path(args.inpath).is_file() and not Path(args.inpath).is_dir(): # BIN input i
             if args.model:
                 outpath = (f"{Path(args.inpath).parent}/model-{Path(args.inpath).stem}/")
             else:
-                outpath = (f"{Path(args.inpath).parent}/{Path(args.inpath).stem}/")
+                if not args.folder == -1:
+                    if not args.file == -1:
+                        outpath = (f"{Path(args.inpath).parent}/")
+                    else:
+                        outpath = (f"{Path(args.inpath).parent}/{Path(args.inpath).stem}_{args.folder}/")
+                else:
+                    outpath = (f"{Path(args.inpath).parent}/{Path(args.inpath).stem}/")
         Path(outpath).mkdir(parents=True,exist_ok=True)
-        unpack(input_file_buffer,outpath,args.model,not args.nolist,args.qbextensions)
+        if not args.folder == -1:
+            if not args.file == -1:
+                extract_file(input_file_buffer,outpath,args.folder,args.file,args.model,not args.nolist,args.qbextensions)
+            else:
+                if args.model and ru32(input_file_buffer,0x08) != 0x20031205:
+                    file_count = 4
+                else:
+                    file_count = ru32(input_file_buffer,0x14+(0x10*args.folder))
+                for i in range(file_count):
+                    extract_file(input_file_buffer,outpath,args.folder,i,args.model,not args.nolist,args.qbextensions,True)
+                print(f"Successfully extracted folder {args.folder}.")
+        else:
+            unpack(input_file_buffer,outpath,args.model,not args.nolist,args.qbextensions)
+            print(f"Unpacked BIN to {outpath}")
         input_file.close()
-        print(f"Unpacked BIN to {outpath}")
 
 
 elif Path(args.inpath).is_dir(): # BIN output is assumed
